043f2a5ee6cafadc758ede9ee03691199d8474da
sundae <yooyeonjun2@gmail.com>
2024-06-07 00:10:44 +0900

    update

--- SoundArchive-TheOrchestra/Assets/Scripts/Note.cs
+++ SoundArchive-TheOrchestra/Assets/Scripts/Note.cs
@@ -1,10 +1,32 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Note : MonoBehaviour
 {
-    private
+    private NoteGenerator note;
+    private Vector3 desPos;
+
+    private Rigidbody2D rigid;
+    [SerializeField] private float speed;
+
+    private void Start()
+    {
+        note = GameObject.Find("NoteGenerator").GetComponent<NoteGenerator>();
+        speed = note.scrollSpeed;
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.down * speed * Time.smoothDeltaTime);
+    }
+
+    public void OnBecameInvisible()
+    {
+        if (gameObject.transform.position.y <= -5f)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
--- SoundArchive-TheOrchestra/Assets/Scripts/NoteGenerator.cs
+++ SoundArchive-TheOrchestra/Assets/Scripts/NoteGenerator.cs
@@ -1,18 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NoteGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Sheet sheet;
+
+    public GameObject notePrefab;
+    
+    private float noteCorrectRate = 0.001f;
+
+    private float notePosY;
+    private float noteStartPosY;
+    private float offset;
+    public float scrollSpeed;
+
+    public bool isGenFin;
+
+    private void Start()
     {
-        
+        scrollSpeed = 17f;
+        sheet = GameObject.Find("Sheet").GetComponent<Sheet>();
+        notePosY = scrollSpeed;
+        noteStartPosY = scrollSpeed * 3.0f;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (isGenFin.Equals(false))
+        {
+            GenNote();
+            isGenFin = true;
+        }
+    }
+
+    private void GenNote()
+    {
+        GenNoteList(sheet.noteList1, notePrefab, new Vector3(-1.5f, 0, 0));
+        GenNoteList(sheet.noteList2, notePrefab, new Vector3(-0.5f, 0, 0));
+        GenNoteList(sheet.noteList3, notePrefab, new Vector3(0.5f, 0, 0));
+        GenNoteList(sheet.noteList4, notePrefab, new Vector3(1.5f, 0, 0));
+    }
+
+    private void GenNoteList(List<float> noteList, GameObject notePrefab, Vector3 positionOffset)
+    {
+        float posY;
+        foreach (float noteTime in noteList)
+        {
+            posY = noteStartPosY + notePosY * (noteTime * noteCorrectRate) + positionOffset.y;
+            Instantiate(notePrefab, new Vector3(positionOffset.x, posY, positionOffset.z), Quaternion.identity);
+        }
     }
 }
