9c67d581280ff5e080032e0733a228133a04e415
sundae <yooyeonjun2@gmail.com>
2024-06-20 20:59:46 +0900

    small update

--- SoundArchive-TheOrchestra/Assets/Scripts/SongManager.cs
+++ SoundArchive-TheOrchestra/Assets/Scripts/SongManager.cs
@@ -6,11 +6,24 @@ using UnityEngine;
 public class SongManager : MonoBehaviour
 {
     public AudioSource music;
+    public AudioClip clip;
     
     public string songName;
+    public bool isGameFin;
 
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        music = GetComponent<AudioSource>();
+    }
+
+    public void PlayAudioForPlay()
+    {
+        music.timeSamples = 0;
+        music.PlayDelayed(3.0f);
     }
 }
